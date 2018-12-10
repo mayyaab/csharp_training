5c0da4a11410dba1ecd6c5aab4b8070c27ff6fd9
mayya <m.abzhigitova@anchorfree.com>
2018-12-09 19:55:00 -0800

    homework3

---
+++ addressbook-web-tests/UnitTestProject1/Figure.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+    class Figure
+    {
+        private bool colored = false;
+
+
+        public bool Colored
+        {
+            get { return colored; }
+            set { colored = value; }
+        }
+    }
+}
